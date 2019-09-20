7aa1a4640346bc569930ed502036593ca0a33882
Jian-Long Huang <huang@jianlong.org>
2019-09-20 13:43:14 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mute Medium Signup Overlay
+
+This script auto closes Medium's annoying signup overlay.
+
+![](doc/screenshot.png)
---
+++ script/app.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Mute Medium Signup Overlay
+// @namespace    https://greasyfork.org/zh-TW/users/79288
+// @version      1.0
+// @description  Mute Medium's annoying signup overlay
+// @author       jlhg
+// @match        https://medium.com/@*
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  let buttons = document.getElementsByTagName("button");
+  if (buttons.length > 0) {
+    for (let i = 0; i < buttons.length; i++) {
+      let button = buttons[i];
+      if (button.querySelector('svg[class="x-29px_svg__svgIcon-use"]')) {
+        button.click();
+      }
+    }
+  }
+})();
